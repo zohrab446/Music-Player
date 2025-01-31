e61bad2362089b656352437ae9058abccc56971e
Zohrab <116635367+zohrab446@users.noreply.github.com>
2025-01-31 12:52:15 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const container = document.querySelector(".container");
 const image = document.querySelector("#music-image");
 const title = document.querySelector("#music-details .title");
 const singer = document.querySelector("#music-details .singer");
-const prev = document.querySelector("#controls #prev");
 const play = document.querySelector("#controls #play");
+const prev = document.querySelector("#controls #prev");
 const next = document.querySelector("#controls #next");
 const duration = document.querySelector("#duration");
 const currentTime = document.querySelector("#current-time");
 const progressBar = document.querySelector("#progress-bar");
+const volume = document.querySelector("#volume");
+const volumeBar = document.querySelector("#volume-bar");
 
 const player = new MusicPlayer(musicList);
 
@@ -28,65 +30,85 @@ play.addEventListener("click", () => {
     isMusicPlay ? pauseMusic() : playMusic();
 });
 
+prev.addEventListener("click", () => { prevMusic(); });
 
-prev.addEventListener("click", () =>{
-    prevMusic();
-});
+next.addEventListener("click", () => { nextMusic(); });
 
-
-next.addEventListener("click", () =>{
-    nextMusic();
-});
-
-function nextMusic(){
-    player.next();
-    let music =player.getMusic();
-    displayMusic(music);
-    playMusic();
-}
-
-function prevMusic(){
+const prevMusic = () => {
     player.prev();
-    let music =player.getMusic();
+    let music = player.getMusic();
     displayMusic(music);
     playMusic();
 }
 
-
-function pauseMusic(){
-  container.classList.remove("playing");
-  play.classList = "fa-solid fa-play";
-  audio.pause();
+const nextMusic = () => {
+    player.next();
+    let music = player.getMusic();
+    displayMusic(music);
+    playMusic();
 }
 
-function playMusic(){
+const pauseMusic = () => {
+    container.classList.remove("playing");
+    play.querySelector("i").classList = "fa-solid fa-play";
+    audio.pause();
+}
+
+const playMusic = () => {
     container.classList.add("playing");
-    play.classList = "fa-solid fa-pause";
+    play.querySelector("i").classList = "fa-solid fa-pause";
     audio.play();
 }
 
 const calculateTime = (toplamSaniye) => {
-    const dakika = Math.floor(toplamSaniye /60);
+    const dakika = Math.floor(toplamSaniye / 60);
     const saniye = Math.floor(toplamSaniye % 60);
-    const guncellenenSaniye = saniye< 10 ? `0${saniye}`:`${saniye}`;
+    const guncellenenSaniye = saniye < 10 ? `0${saniye}`: `${saniye}`;
     const sonuc = `${dakika}:${guncellenenSaniye}`;
     return sonuc;
 }
 
-
-audio.addEventListener("loadedmetadata",() =>{
-   duration.textContent = calculateTime(audio.duration);
-   progressBar.max = Math.floor(audio.duration);
-   progressBar.step = 1;
+audio.addEventListener("loadedmetadata", () => {
+    duration.textContent = calculateTime(audio.duration);
+    progressBar.max = Math.floor(audio.duration);
 });
 
-
-audio.addEventListener("timeupdate", () =>{
-  progressBar.value = Math.floor(audio.currentTime);
-  currentTime.textContent = calculateTime(progressBar.value);
+audio.addEventListener("timeupdate", () => {
+    progressBar.value = Math.floor(audio.currentTime);
+    currentTime.textContent = calculateTime(progressBar.value);
 });
 
 progressBar.addEventListener("input", () => {
-   currentTime.textContent = calculateTime(progressBar.value);
-   audio.currentTime = progressBar.value;
+    currentTime.textContent = calculateTime(progressBar.value);
+    audio.currentTime = progressBar.value;
+});
+
+let sesDurumu = "sesli";
+
+volumeBar.addEventListener("input", (e) => {
+    const value = e.target.value;
+    audio.volume = value / 100;
+    if(value == 0) {
+        audio.muted = true;
+        sesDurumu = "sessiz";
+        volume.classList = "fa-solid fa-volume-xmark";
+    } else {
+        audio.muted = false;
+        sesDurumu = "sesli";
+        volume.classList = "fa-solid fa-volume-high";
+    }
+});
+
+volume.addEventListener("click", () => {
+    if(sesDurumu==="sesli") {
+        audio.muted = true;
+        sesDurumu = "sessiz";
+        volume.classList = "fa-solid fa-volume-xmark";
+        volumeBar.value = 0;
+    } else {
+        audio.muted = false;
+        sesDurumu = "sesli";
+        volume.classList = "fa-solid fa-volume-high";
+        volumeBar.value = 100;
+    }
 });
--- musicplayer.js
+++ musicplayer.js
@@ -1,7 +1,7 @@
 class MusicPlayer {
     constructor(musicList) {
         this.musicList = musicList;
-        this.index = 0;
+        this.index = 1;
     }
 
     getMusic() {
